84a7c8cc38d9e2b4771c3870aa91cfd1be0a09c8
lbj <luofatso@gmail.com>
2023-07-21 17:55:01 +0800

    feat: 添加旅行 巴朗山

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -161,6 +161,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       text: '旅行',
       collapsed: true,
       items: [
+        { text: '巴朗山', link: '/关于生活/旅行/巴朗山' },
         { text: '四姑娘山-双桥沟', link: '/关于生活/旅行/四姑娘山-双桥沟' },
         { text: '康定木格措', link: '/关于生活/旅行/康定-木格措' },
         { text: '大邑龙窝子', link: '/关于生活/旅行/大邑龙窝子' },
