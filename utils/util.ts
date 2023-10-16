18cfcd872107bc8a794d790b15e7497b94b70837
lbj <luofatso@gmail.com>
2023-10-16 15:51:50 +0800

    adjust: 调整文件路径

--- utils/util.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-bitwise
-export const getRandom = (max: number, min = 0) => ~~(Math.random() * (max - min)) + min
-// eslint-disable-next-line no-bitwise
-export const random = (max: number, min = 0) => ~~(Math.random() * (max - min)) + min
