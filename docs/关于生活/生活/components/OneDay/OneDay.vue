0d670730f32150a9810a795fefb8ee4a97094c98
罗保军 <luofatso@gmail.com>
2024-04-26 11:04:01 +0800

    feat: 追加一点2024

--- docs/关于生活/生活/components/OneDay/OneDay.vue
+++ docs/关于生活/生活/components/OneDay/OneDay.vue
@@ -2,7 +2,7 @@
   <h2 id="今日一点" tabindex="-1">今日一点 <a class="header-anchor" href="#今日一点" aria-hidden="true">#</a></h2>
   <div class="one-day d-flex aling-center justify-center flex-column ">
     {{ todayOne }}
-    <p class="intro text-c"> <strong>每日一点</strong> 已连续坚持 <span>{{ OneDayAll.length }}</span> 天</p>
+    <p class="intro text-c"> <strong>每日一点</strong> 已坚持发布 <span>{{ OneDayAll.length }}</span> 天</p>
   </div>
   <h2 id="全部一点" tabindex="-1">全部一点 <a class="header-anchor" href="#全部一点" aria-hidden="true">#</a></h2>
   <ul>
@@ -13,9 +13,10 @@
 </template>
 <script setup lang='ts'>
 import OneDay2023 from './OneDay2023'
+import OneDay2024 from './OneDay2024'
 import { getRandom } from '../../../../utils/util'
 
-const OneDayAll = OneDay2023.reverse()
+const OneDayAll = [...OneDay2024.reverse(), ...OneDay2023.reverse()]
 
 const todayOne = OneDayAll[getRandom(OneDayAll.length)]
 
