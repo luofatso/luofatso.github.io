b180da21c7a5474cdf324931b1342c56079ba78a
罗保军 <luofatso@gmail.com>
2024-05-23 18:15:20 +0800

    feat: 添加一些东西

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -228,7 +228,8 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
         { text: 'BMI计算器', link: '/关于生活/小工具/bmi' },
         { text: '阴阳历查询', link: '/关于生活/小工具/阴阳历查询' },
         { text: '倒计时查询', link: '/关于生活/小工具/date' },
-        { text: '吃什么', link: '/关于生活/小工具/eat' }
+        { text: '吃什么', link: '/关于生活/小工具/eat' },
+        { text: '随机彩票', link: '/前端其它/随机彩票' }
       ]
     },
     {
