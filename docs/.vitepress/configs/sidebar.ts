15d55fe20ab49ec8a41c593bf9a02f947246c572
luofatso <luofatso@gmail.com>
2024-05-27 22:28:20 +0800

    feat: 添加旅行-九峰山

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -192,6 +192,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       text: '旅行',
       collapsed: true,
       items: [
+        { text: '彭州-九峰山', link: '/关于生活/旅行/彭州-九峰山' },
         { text: '大邑-季禅沟', link: '/关于生活/旅行/大邑-季禅沟' },
         { text: '彭州-狮子山', link: '/关于生活/旅行/彭州-狮子山' },
         { text: '成都-天府-鹅项岭', link: '/关于生活/旅行/成都-天府-鹅项岭' },
