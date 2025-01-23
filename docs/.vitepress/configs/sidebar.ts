5419de281972ccb091b1f0924b1f38a8bc17aae9
luobaojun <luofatso@gmail.com>
2025-01-23 16:25:32 +0800

    feat: 添加混沌

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -7,6 +7,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       // collapsible: true,
       collapsed: true,
       items: [
+        { text: '混沌', link: '/个人随笔/梦呓/混沌' },
         { text: '稻香', link: '/个人随笔/梦呓/稻香' },
         { text: '梦呓', link: '/个人随笔/梦呓/梦呓' },
         { text: '窗外', link: '/个人随笔/梦呓/窗外' },
